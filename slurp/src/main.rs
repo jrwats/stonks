4daee641313fd5752b80efc4635fc77c850aa1e4
John Watson <jrwats@gmail.com>
2022-06-25 17:27:51 -0700

    EMA tweak

--- slurp/src/main.rs
+++ slurp/src/main.rs
@@ -258,11 +258,23 @@ impl Wrapper {
     }
 
     fn calculate_exp_moving_avgs(window: usize, quotes: &[QuoteRow]) -> Vec<(i32, f64)> {
-        let sum: f64 = quotes[0..window].iter().map(|q| q.quote.close).sum();
-        let mut avg: f64 = sum / (window as f64);
+        if quotes.is_empty() || window > quotes.len() {
+            return vec![];
+        }
+        let mut avg: f64 = quotes[0].quote.close;
         let mut avgs = Vec::with_capacity(quotes.len() - window);
-        avgs.push((quotes[window - 1].id, avg));
+
+        // Initialize our EMA with a pseudo-EMA, pretending first entry represents an average, and
+        // extending the window until it matches
+        let mut init_window = 2f64;
+        for quote in quotes[1..window].iter() {
+            let init_smoothing = 2.0 / (init_window + 1.0);
+            avg = (quote.quote.close - avg) * init_smoothing + avg;
+            init_window += 1.0;
+        }
+
         let smoothing: f64 = 2.0 / (window as f64 + 1.0);
+        avgs.push((quotes[window - 1].id, avg));
         for quote in quotes[window..].iter() {
             avg = (quote.quote.close - avg) * smoothing + avg;
             avgs.push((quote.id, avg));
@@ -348,15 +360,14 @@ mod test {
             .into_iter()
             .collect();
         let avgs = Wrapper::calculate_exp_moving_avgs(3, &mocks);
-        // [(2, 3.0), (3, 4.25), (4, 5.125), (5, 6.0625)]
         assert_eq!(
             avgs,
-            vec![
-                (2, 3.0),
-                (3, 3.0 + 0.5 * 2.5),
-                (4, 4.25 + 1.75 * 0.5),
-                (5, 5.125 + 1.875 * 0.5)
-            ]
+            [
+                (2, 3.333333333333333),
+                (3, 4.416666666666666),
+                (4, 5.208333333333333),
+                (5, 6.104166666666666)
+            ],
         );
     }
 }
